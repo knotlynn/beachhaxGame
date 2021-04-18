d7ef4824dc640a0cf1d2d88de71643e3ee58a5c1
Brenna Curtis <bniccurtis@gmail.com>
2021-04-18 01:31:55 -0500

    it's a shame we're wiping this as soon as it get's pushed through..

--- game/myScript.js
+++ game/myScript.js
@@ -1,4 +1,29 @@
-var c = document.getElementById("game");
-var ctx = c.getContext("2d");
+var canvas = document.getElementById("game"); //find canvas object
+var ctx = canvas.getContext("2d"); //create drawing object
 
-ctx.fillRect(0,0, 800, 600);
+function startGame() {
+  myGameArea.start();
+  myGamePiece = new component(30, 30, "red", 10, 120);
+}
+
+var myGameArea = {
+  canvas: document.createElement("canvas"),
+  start: function () {
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  }
+};
+
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  ctx = myGameArea.context;
+  ctx.fillStyle = color;
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+
+startGame();
